5ad25f414314ddae6d380bed7f890bcf34068a28
Edu Caselles <veducm@gmail.com>
2016-11-19 20:28:05 +0000

    Tests Cell model.

--- GameOfLifeTests/CellSpec.swift
+++ GameOfLifeTests/CellSpec.swift
@@ -8,47 +8,58 @@
 
 import Quick
 import Nimble
+@testable import GameOfLife
 
 class CellSpec: QuickSpec {
     override func spec() {
         
-        describe("nextEvolution") {
+        describe(".nextEvolution") {
             context("when the cell is live") {
+                let cell = Cell.Live
+                
                 context("and it has less than 2 neighbours") {
                     it("will be dead") {
-                        
+                        let result = cell.nextEvolution(forNeighboursCount: 1)
+                        expect(result).to(equal(Cell.Dead))
                     }
                 }
                 
                 context("and it has 2 neighbours") {
                     it("will be live") {
-                        
+                        let result = cell.nextEvolution(forNeighboursCount: 2)
+                        expect(result).to(equal(Cell.Live))
                     }
                 }
                 
                 context("and it has 3 neighbours") {
                     it("will be live") {
-                        
+                        let result = cell.nextEvolution(forNeighboursCount: 3)
+                        expect(result).to(equal(Cell.Live))
                     }
                 }
                 
                 context("and it has more than 3 neighbours") {
                     it("will be dead") {
-                        
+                        let result = cell.nextEvolution(forNeighboursCount: 4)
+                        expect(result).to(equal(Cell.Dead))
                     }
                 }
             }
             
             context("when the cell is dead") {
+                let cell = Cell.Dead
+                
                 context("and it has 3 neighbours") {
                     it("will be live") {
-                        
+                        let result = cell.nextEvolution(forNeighboursCount: 3)
+                        expect(result).to(equal(Cell.Live))
                     }
                 }
                 
                 context("and it does not have 3 neighbours") {
                     it("will be dead") {
-                        
+                        let result = cell.nextEvolution(forNeighboursCount: 2)
+                        expect(result).to(equal(Cell.Dead))
                     }
                 }
             }
