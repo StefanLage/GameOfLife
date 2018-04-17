14624fa2ae2930312be0244c322d2594d54e9d89
Stefan Lage <lagestfan@gmail.com>
2018-04-17 11:48:44 +0100

    Remove some tests

--- GameOfLifeUITests/GameOfLifeUITests.swift
+++ GameOfLifeUITests/GameOfLifeUITests.swift
@@ -31,23 +31,6 @@ class GameOfLifeUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-
-        let app = XCUIApplication()
-        XCTAssertTrue(app.tables.cells.staticTexts["Toad"].exists)
-        XCTAssertTrue(app.tables.cells.staticTexts["Blinker"].exists)
-        XCTAssertTrue(app.tables.cells.staticTexts["Pulsar"].exists)
-        XCTAssertTrue(app.tables.cells.staticTexts["Beacon"].exists)
-
-        let tablesQuery = app.tables
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Toad"]/*[[".cells.staticTexts[\"Toad\"]",".staticTexts[\"Toad\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-
-        let collectionViewsQuery = app.collectionViews
-        collectionViewsQuery.children(matching: .cell).element(boundBy: 7).children(matching: .other).element.tap()
-        collectionViewsQuery.children(matching: .cell).element(boundBy: 8).children(matching: .other).element.tap()
-        app.navigationBars["Toad"].buttons["Game of Life"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Beacon"]/*[[".cells.staticTexts[\"Beacon\"]",".staticTexts[\"Beacon\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["Beacon"].buttons["Game of Life"].tap()
-
     }
     
 }
